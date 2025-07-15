5ef9c9f94abf2922e3764837fc457d5fe9097b14
Dodikz <sabdoilahidodik735@gmail.com>
2025-07-15 12:43:41 +0700

    set up user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+//user
+Route::prefix('/')->group(function () {
+
 });
+
+Route::prefix('admin')->group(function () {
+
+});
+
